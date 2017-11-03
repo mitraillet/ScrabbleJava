84f909f2a8de2c3b5ef025ca22db807abffd9edc
Simon Fauconnier <32515627+simon-fauconnier@users.noreply.github.com>
2017-11-03 20:52:05 +0100

    Ajout des classes Joker, Lettre(complet), joueur

---
+++ scrabble/Joker.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package scrabble;
+
+/**
+ * @author Fauconnier/Henriquet
+ *
+ */
+public class Joker extends Lettre {
+
+	/**
+	 * Permet de définir la valeur du libellé
+	 */
+	public void setNom() {
+		this.nom = 'h';
+	}
+	
+	public Joker(char nom, int valeur, int instance) {
+		super();
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- scrabble/Lettre.java
+++ scrabble/Lettre.java
@@ -9,11 +9,42 @@ package scrabble;
  */
 public class Lettre {
 
+	/**
+	 * Le libellé du caractère
+	 */
 	char nom;
+	
+	/**
+	 * La valeur de la lettre (score)
+	 */
 	int valeur;
+	
+	/**
+	 * Le nombre d'instance de la lettre dans le sac
+	 */
 	int instance;
 	
+	/**
+	 * Constructeur
+	 * @param nom le nom de la lettre
+	 * @param valeur la valeur de la lettre
+	 * @param instance le nombre d'instance dans le sac
+	 */
+	public Lettre(char nom, int valeur, int instance) {
+		this.nom = nom;
+		this.valeur = valeur;
+		this.instance = instance;
+	}
 	
+	/**
+	 * Constructeur par défaut
+	 */
+	public Lettre() {
+		this.nom = 'a';
+		this.valeur = 0;
+		this.instance = 0;
+	}
+
 	/**
 	 * @param args
 	 */
