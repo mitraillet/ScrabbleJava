66a45c2bdbba841a0cadedcbd1c12fd014be3bcf
Simon Fauconnier <32515627+simon-fauconnier@users.noreply.github.com>
2017-11-03 11:49:32 +0100

    Add files via upload

---
+++ scrabble/Lettre.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package scrabble;
+
+/**
+ * @author Fauconnier/Henriquet
+ *
+ */
+public class Lettre {
+
+	char nom;
+	int valeur;
+	int instance;
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
