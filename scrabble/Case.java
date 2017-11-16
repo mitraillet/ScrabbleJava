27f6cd0e40a100cc86d891936b17f56dad86ec99
mitraillet <steve.henriquet@gmail.com>
2017-11-16 20:32:09 +0100

    MàJ de fichier

--- scrabble/Case.java
+++ scrabble/Case.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Classe case
  */
 package scrabble;
 
@@ -10,55 +10,65 @@ package scrabble;
 public class Case {
 	
 	/**
-	 * Objet Lettre à importer  et surtout mettre à jour dans la case
+	 * Contient la lettre "posée" sur la case
 	 */
-	private Lettre lettre;
-	/** bonus serait un chiffre entre 0 et 5 pour chaque "bonus" 
+	protected Lettre lettre;
+	
+	/** 
+	 * Définis le bonus de la case, selon le code suivant :
 	 *  0 = sans bonus
 	 *  1 = lettre double
 	 *  2 = lettre triple
 	 *  3 = mot double
 	 *  4 = mot triple
-	 *  5 = unique la première case par où doit passer le premier mot
+	 *  5 = (unique) la case du millieu, où doit passer le premier mot
 	 */
 	private int bonus;	
 	
+	
 	/**
-	 * Constructeur de Case par défaut
+	 * @return lettre la lettre contenue dans la case
 	 */
-	public Case() {
-		this(0);
-	}
-	/**
-	 * Constructeur de Case
-	 * @param bonus associé à la case
-	 */
-	public Case(int bonus) {
-		this.lettre = null;
-		this.bonus = bonus;
-	}
-	/**
-	 * @return la lettre contenue dans la case
-	 */
-	protected Lettre getLettre() {
+	public Lettre getLettre() {
 		return lettre;
 	}
+	
 	/**
-	 * @param lettre l'objet à mettre dans la case 
+	 * @param lettre l'objet lettre à mettre dans la case 
 	 */
-	protected void setLettre(Lettre lettre) {
+	public void setLettre(Lettre lettre) {
 		this.lettre = lettre;
 	}
+	
 	/**
-	 * @return la valeur du bonus 
+	 * @return bonus l'indice du bonus 
 	 */
 	public int getBonus() {
 		return this.bonus;
 	}
+	
 	/**
 	 * @param bonus le chiffre du bonus pour la case
 	 */
-	protected void setBonus(int bonus) {
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	
+	
+	
+	/**
+	 * Constructeur de l'objet Case par défaut (sans paramètre)
+	 */
+	public Case() {
+		this(0);
+	}
+	
+	/**
+	 * Constructeur de l'objet Case
+	 * @param bonus le bonus associé à la case
+	 */
+	public Case(int bonus) {
+		this.lettre = null;
 		this.bonus = bonus;
 	}
 	
