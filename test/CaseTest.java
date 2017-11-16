27dc64b0f94daf52aeb9029704e79a5fd7233068
Simon Fauconnier <32515627+simon-fauconnier@users.noreply.github.com>
2017-11-16 20:22:01 +0100

    Ajout test case

---
+++ test/CaseTest.java
@@ -0,0 +1,86 @@
+/**
+ * Test unitaire pour la classe Case
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import scrabble.Case;
+import scrabble.Lettre;
+
+import org.junit.Test;
+
+public class CaseTest {
+
+	
+	/**
+	 * Test du constructeur sans paramètre (bonus initialisé à 0)
+	 */
+	@Test
+	public void testCase() {
+		//fail("Not yet implemented"); // TODO
+		Case caseTest = new Case();
+		assertEquals(caseTest.getBonus(), 0);
+	}
+
+	
+	/**
+	 * Test constructeur avec paramètre (pour le bonus)
+	 */
+	@Test
+	public void testCaseInt() {
+		//fail("Not yet implemented"); // TODO
+		Case caseTest = new Case(3);
+		assertEquals(caseTest.getBonus(), 3);
+	}
+
+	
+	/**
+	 * Test getter de Lettre
+	 */
+	@Test
+	public void testGetLettre() {
+		//fail("Not yet implemented"); // TODO
+		Case caseTest = new Case();
+		Lettre testLettre = new Lettre('a', 1);
+		caseTest.setLettre(testLettre);
+		assertEquals(testLettre, caseTest.getLettre());
+	}
+
+	/**
+	 * Test du setter de Lettre
+	 */
+	@Test
+	public void testSetLettre() {
+		//fail("Not yet implemented"); // TODO
+		Case caseTest = new Case();
+		Lettre testLettre = new Lettre('a', 1);
+		caseTest.setLettre(testLettre);
+		assertEquals(caseTest.getLettre().getLabel(), 'a');
+	}
+
+	
+	/**
+	 * Test du getter de bonus
+	 */
+	@Test
+	public void testGetBonus() {
+		//fail("Not yet implemented"); // TODO
+		Case caseTest2 = new Case();
+		assertEquals(caseTest2.getBonus(), 0);
+		Case caseTest3 = new Case(3);
+		assertEquals(caseTest3.getBonus(), 3);
+	}
+
+	/**
+	 * Test du setter de bonus
+	 */
+	@Test
+	public void testSetBonus() {
+		//fail("Not yet implemented"); // TODO
+		Case caseTest = new Case();
+		caseTest.setBonus(1);
+		assertEquals(caseTest.getBonus(), 1);
+	}
+
+}
