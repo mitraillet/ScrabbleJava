da18abdf7b5b940a26dbf0d5bc3bf9df8983f36b
Simon Fauconnier <32515627+simon-fauconnier@users.noreply.github.com>
2017-11-20 14:37:52 +0100

    Nettoyage code (commentaire)

--- scrabble/SimpleErrorHandler.java
+++ scrabble/SimpleErrorHandler.java
@@ -10,18 +10,36 @@ import org.xml.sax.SAXParseException;
 
 /**
  * @author Fauconnier
- * Erreur XML
+ * Erreur de lecture XML
  */
 public class SimpleErrorHandler implements ErrorHandler { 
+    
+    //JAXP = Java API for XML Processing
+    
+    /**
+    * Récupère les avertissements lors de la lecture d'un XML
+    * @param e exception du package JAXP
+    * @throws erreur du package JAXP
+    */
     public void warning(SAXParseException e) throws SAXException {
         System.out.println("WARNING : " + e.getMessage());
     }
 
+    /**
+    * Récupère les erreurs lors de la lecture d'un XML
+    * @param e exception du package JAXP
+    * @throws erreur du package JAXP
+    */
     public void error(SAXParseException e) throws SAXException {
         System.out.println("ERROR : " + e.getMessage());
         throw e;
     }
 
+    /**
+    * Récupère les erreurs amenant au crash, lors de la lecture d'un XML
+    * @param e exception du package JAXP
+    * @throws erreur du package JAXP
+    */
     public void fatalError(SAXParseException e) throws SAXException {
         System.out.println("FATAL ERROR : " + e.getMessage());
         throw e;
