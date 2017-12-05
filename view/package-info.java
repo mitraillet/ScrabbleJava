dff1b7e142983335faae86b7a1015a297c384e2d
Simon Fauconnier <32515627+simon-fauconnier@users.noreply.github.com>
2017-12-05 12:24:08 +0100

    Ajout des package-info

---
+++ view/package-info.java
@@ -0,0 +1,12 @@
+/**
+*Ce package contient toutes les views (GUI + Console) :
+*<ul>
+*<li>La classe <code>Bouton.java</code></li>
+*<li>La classe <code>ScrabbleView.java</code></li>
+*<li>La classe <code>ScrabbleViewConsole.java</code></li>
+*<li>La classe <code>ScrabbleViewGUI.java</code></li>
+*</ul>
+*
+*@author Fauconnier/Henriquet
+*/
+package view;
